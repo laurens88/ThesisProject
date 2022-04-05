6df7236c2046d50fc4380bd5e838c5b0ec44edef
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2022-04-05 12:08:32 +0200

    Tweets pulled, 7512 total tweets

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import time
 
 
 def main():
-    i = 0
-    while i < 10:
-        time.sleep(900)
-        request.request(100)  # Pull
-        request.merge()  # Merge json tweet files
-        i = i+1
-    #label.convert()  # Prepare for lighttag
+    # i = 0
+    # while i < 10:
+    #     request.request(100)  # Pull
+    #     request.merge()  # Merge json tweet files
+    #     i = i+1
+    #     time.sleep(600)
+    label.convert()  # Prepare for lighttag
     #label.insert_labels()  # Finalize data
 
 
