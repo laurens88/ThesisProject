854039b1bd3242c749227174381513e29283b429
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2022-05-10 17:55:26 +0200

    train test and val set are generated in correct json format for bert model use

--- main.py
+++ main.py
@@ -23,12 +23,16 @@ def main():
     clean_data = Preprocessor.process_data()  # Data with preprocessing
     clean_X = clean_data['X']
     clean_y = clean_data['y']
-    x_train, x_test, x_val, y_train, y_test, y_val = data.train_test_validate_split(clean_X, clean_y)
+    # x_train, x_test, x_val, y_train, y_test, y_val = data.train_test_validate_split(clean_X, clean_y)
+    #
+    # data.data_to_model_format(clean_X, clean_y, "complete_set.json")
+    # data.data_to_model_format(x_train, y_train, "train.json")
+    # data.data_to_model_format(x_test, y_test, "test.json")
+    # data.data_to_model_format(x_val, y_val, "val.json")
 
-    data.data_to_model_format(clean_X, clean_y, "complete_set.json")
-    data.data_to_model_format(x_train, y_train, "train_set.json")
-    data.data_to_model_format(x_test, y_test, "test_set.json")
-    data.data_to_model_format(x_val, y_val, "val_set.json")
+    model1, model2 = model.classify(clean_X)
+    model.label_dist(model1)
+    model.label_dist(model2)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -3,8 +3,24 @@ from transformers import pipeline, BertTokenizer
 
 
 def classify(tweets):
-    sentiment_pipeline = pipeline(task="sentiment-analysis", model="finiteautomata/bertweet-base-sentiment-analysis")
-    return sentiment_pipeline(tweets)
+    sentiment_pipeline_base = pipeline(task="sentiment-analysis", model="finiteautomata/bertweet-base-sentiment-analysis")
+    sentiment_pipeline_preprocessed = pipeline(task="sentiment-analysis", model="laurens88/finetuning-crypto-tweet-sentiment-test")
+    return sentiment_pipeline_base(tweets), sentiment_pipeline_preprocessed(tweets)
+
+
+def label_dist(model):
+    labels = model
+    pos, neg, neu = 0, 0, 0
+    for l in labels:
+        label = l['label']
+        if label == "POS":
+            pos += 1
+        elif label == "NEG":
+            neg += 1
+        elif label == "NEU":
+            neu += 1
+    print("Base bert results:")
+    print(f"Positive: {pos}\nNeutral: {neu} \nNegative: {neg} \n")
 
 
 def bert_tokenize(data):
